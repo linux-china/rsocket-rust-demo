14d8953f466cd3b5e5b29512229057f3cfda355a
linux_china <libing.chen@gmail.com>
2020-08-22 12:02:53 -0700

    rsocket client

---
+++ src/client.rs
@@ -0,0 +1,21 @@
+use rsocket_rust::prelude::*;
+use rsocket_rust_transport_tcp::TcpClientTransport;
+
+#[tokio::main]
+async fn main() {
+    let cli = RSocketFactory::connect()
+        .acceptor(Box::new(|| Box::new(EchoRSocket)))
+        .transport(TcpClientTransport::from("127.0.0.1:7878"))
+        .setup(Payload::from("READY!"))
+        .mime_type("text/plain", "text/plain")
+        .start()
+        .await
+        .unwrap();
+    let req = Payload::builder()
+        .set_data_utf8("Hello World!")
+        .set_metadata_utf8("Rust")
+        .build();
+    let res = cli.request_response(req).await.unwrap();
+    println!("got: {:?}", res);
+    cli.close();
+}
